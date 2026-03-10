1fe617d67fc18e7182c52c11709753c035e934ec
Eliam2102 <157552853+Eliam2102@users.noreply.github.com>
2026-03-10 16:38:59 -0600

    FIX - PRISMA

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,9 @@
 import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
+
+const { PrismaClient } = pkg
 
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
